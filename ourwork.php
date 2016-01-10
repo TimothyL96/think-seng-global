000fd978b95ab956ba37d45503f2220d71bb4065
timothy Lam <timothy Lam>
2016-01-10 19:14:57 +0800

    paypal button

--- ourwork.php
+++ ourwork.php
@@ -32,28 +32,35 @@
 			Day 1 - Fundamental analysis and Economics</br>
 			Day 2 - Technical analysis and Stock Pick method</br></br>
 			Advance share trading</br>
-			Short Term trading and portfolio management</br>
+Short Term trading and portfolio management</br>Fees: RM 2800</br>Pay Now! :
 			</p>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="RPQ5ZVUFXGN32">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+
 		    <div class="content_container">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>
+		      <p>text</p>
 		    	<div class="button_small">
 		        <a href="#">Read more</a>
 		      </div><!--close button_small-->
 		    </div><!--close content_container-->
             <div class="content_container">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>          
+		      <p>text</p>
 		    	<div class="button_small">
 		        <a href="#">Read more</a>
 		        </div><!--close button_small-->		  
 		    </div><!--close content_container-->
 		    <div class="content_container">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>
+		      <p>text3</p>
 		    	<div class="button_small">
 		        <a href="#">Read more</a>
 		      </div><!--close button_small-->
 		    </div><!--close content_container-->
             <div class="content_container">
-		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque cursus tempor enim. Aliquam facilisis neque non nunc posuere eget volutpat metus tincidunt.</p>          
+		      <p>text4</p>
 		    	<div class="button_small">
 		        <a href="#">Read more</a>
 		        </div><!--close button_small-->		  
