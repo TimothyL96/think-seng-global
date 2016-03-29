b237f1b20deb0eb0a7d84ee354a5cbcb2e73144a
Travis <travis@travis-ci.org>
2016-03-29 09:38:14 +0800

    a

--- register.php
+++ register.php
@@ -1,22 +1,15 @@
-<?php
-    include("header.html");
-?>
-           <html>
-               document.getElementById("MyElement").className = "MyClass";
-
-           </html>
-o
-	<?php
-		include("sidebar.html");
-	?>
+<?php include("header.html"); ?>
+        <script>
+            document.getElementById("register").addClass = "current";
+        </script>
+<?php include("sidebar.html"); ?>
 
 	  <div id="content">
         <div class="content_item">
           <h2>About Us</h2>
-		    <p>REGISTER NOW!. </p>
-
+		    <p>REGISTER NOW! </p>
 			<p>E-mail : enquire@thinksengglobal.com</p>
-			<p>Address : 76-1, Jalan Suria 2, Taman Malim Jaya, 75250, Melaka</p>
+			<p>form:</p>
 	    </div><!--close content_item-->
       </div><!--close content-->
 	</div><!--close site_content-->
