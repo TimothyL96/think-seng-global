e1bfc9331da5afeb3be5c23e2875ea99c623d90d
Travis <travis@travis-ci.org>
2016-03-30 12:29:07 +0800

    formdraft

--- register.php
+++ register.php
@@ -10,10 +10,15 @@
 
 	  <div id="content">
         <div class="content_item">
-          <h2>About Us</h2>
+          <h2>Register</h2>
 		    <p>REGISTER NOW! </p>
 			<p>E-mail : enquire@thinksengglobal.com</p>
 			<p>form:</p>
+            <form action="" method="post">
+                First Name: <input type="text" name="name" placeholder="First Name" required="required"/>
+                Second Name: <input type="text" name="name" placeholder="Second Name" required="required"/>
+                <input type="submit" name="submit" value="Register"/>
+            </form>
 	    </div><!--close content_item-->
       </div><!--close content-->
 	</div><!--close site_content-->
